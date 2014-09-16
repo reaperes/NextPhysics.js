35dd113ad7c2ab792e943a1e389326ee6a8aa387
reaperes <emerald105@hanmail.net>
2014-09-16 12:51:26 +0900

    update controlling camera

--- src/nextphysics/NextPhysics.js
+++ src/nextphysics/NextPhysics.js
@@ -29,11 +29,11 @@ NextPhysics = function (canvasContainer) {
    ****************************************************/
 
   this.update = function () {
-    handleKey();
     engine.update(deltaT);
   };
 
   this.render = function () {
+    controls.update();
     renderer.render();
   };
 
@@ -108,169 +108,14 @@ NextPhysics = function (canvasContainer) {
   /****************************************************
    * Control camera
    ****************************************************/
-  var camera = renderer.camera;
-  var cameraLookPosition = renderer.scene.position;
-  var radius = 20;
-  var theta = 0;
-  var phi = 10;
-
-  //          w 87 119
-  // a 65 97  s 83 115  d 68 100
-  var KEY_UP = 0;
-  var KEY_DOWN = 2;
-  var KEY_LEFT = 1;
-  var KEY_RIGHT = 5;
-  var pressedKeys = {
-    0: false,
-    2: false,
-    1: false,
-    5: false
-  };
-
-  // camera focus
-  var geometry = new THREE.SphereGeometry(0.5);
-  var material = new THREE.MeshBasicMaterial({transparent:true,opacity:0.5,color:0xffffff});
-  var sphere = new THREE.Mesh(geometry, material);
-  sphere.position.set(cameraLookPosition.x, cameraLookPosition.y, cameraLookPosition.z);
-  renderer.scene.add(sphere);
-
-  var cameraVel = .5;
-  function handleKey() {
-    if (pressedKeys[KEY_UP] != pressedKeys[KEY_DOWN]) {
-      var dy = cameraVel;
-
-      if (pressedKeys[KEY_UP]) {
-        camera.position.y += dy;
-        cameraLookPosition.y += dy;
-      }
-      else {
-        camera.position.y -= dy;
-        cameraLookPosition.y -= dy;
-      }
-      sphere.position.set(cameraLookPosition.x, cameraLookPosition.y, cameraLookPosition.z);
-    }
-    if (pressedKeys[KEY_LEFT] != pressedKeys[KEY_RIGHT]) {
-      var dx = -cameraVel * Math.cos(theta * Math.PI / 360);
-      var dz = cameraVel * Math.sin(theta * Math.PI / 360);
-
-      if (pressedKeys[KEY_LEFT]) {
-        camera.position.x += dx;
-        cameraLookPosition.x += dx;
-        camera.position.z += dz;
-        cameraLookPosition.z += dz;
-      }
-      else {
-        camera.position.x -= dx;
-        cameraLookPosition.x -= dx;
-        camera.position.z -= dz;
-        cameraLookPosition.z -= dz;
-      }
-      sphere.position.set(cameraLookPosition.x, cameraLookPosition.y, cameraLookPosition.z);
-    }
-  }
-
-  (function() {
-    camera.position.x = radius * Math.sin(theta * Math.PI / 360) * Math.cos(phi * Math.PI / 360);
-    camera.position.y = radius * Math.sin(phi * Math.PI / 360);
-    camera.position.z = radius * Math.cos(theta * Math.PI / 360) * Math.cos(phi * Math.PI / 360);
-    camera.lookAt(cameraLookPosition);
-
-    var isMouseDown, onMouseDownTheta, onMouseDownPhi;
-    var onMouseDownPosition = new THREE.Vector2();
-    canvasContainer.addEventListener('mousedown', function (event) {
-      event.preventDefault();
-      isMouseDown = true;
-      onMouseDownTheta = theta;
-      onMouseDownPhi = phi;
-      onMouseDownPosition.x = event.pageX;
-      onMouseDownPosition.y = event.pageY;
-    }, false);
-
-    canvasContainer.addEventListener('mousemove', function (event) {
-      event.preventDefault();
-      if (!isMouseDown) return;
-
-      theta = -(event.pageX - onMouseDownPosition.x) * 1.5 + onMouseDownTheta;
-      phi = (event.clientY - onMouseDownPosition.y) * 1.5 + onMouseDownPhi;
-      camera.position.x = cameraLookPosition.x + radius * Math.sin(theta * Math.PI / 360) * Math.cos(phi * Math.PI / 360);
-      camera.position.y = cameraLookPosition.y +radius * Math.sin(phi * Math.PI / 360);
-      camera.position.z = cameraLookPosition.z +radius * Math.cos(theta * Math.PI / 360) * Math.cos(phi * Math.PI / 360);
-      //    camera.updateProjectionMatrix();
-      camera.lookAt(cameraLookPosition);
-    }, false);
-
-    canvasContainer.addEventListener('mouseup', function (event) {
-      event.preventDefault();
-      isMouseDown = false;
-      onMouseDownPosition.x = event.pageX - onMouseDownPosition.x;
-      onMouseDownPosition.y = event.pageY - onMouseDownPosition.y;
-    }, false);
-
-    canvasContainer.addEventListener('mouseover', function (e) {
-    }.bind(this), false);
-    canvasContainer.addEventListener('mousewheel', function (event) {
-      event.preventDefault();
-
-      var wheelDistance = camera.position.distanceTo(cameraLookPosition) * 0.1;
-      if (event.wheelDelta > 0) {
-        camera.position.x -= wheelDistance * Math.sin(theta * Math.PI / 360) * Math.cos(phi * Math.PI / 360);
-        camera.position.y -= wheelDistance * Math.sin(phi * Math.PI / 360);
-        camera.position.z -= wheelDistance * Math.cos(theta * Math.PI / 360) * Math.cos(phi * Math.PI / 360);
-        radius -= wheelDistance;
-      }
-      else {
-        camera.position.x += wheelDistance * Math.sin(theta * Math.PI / 360) * Math.cos(phi * Math.PI / 360);
-        camera.position.y += wheelDistance * Math.sin(phi * Math.PI / 360);
-        camera.position.z += wheelDistance * Math.cos(theta * Math.PI / 360) * Math.cos(phi * Math.PI / 360);
-        radius += wheelDistance;
-      }
-    }, false);
-
-    /****************************************************
-     * Keyboard handling
-     ****************************************************/
-    window.addEventListener('keydown', function (e) {
-      switch (e.keyCode) {
-        case 87:
-        case 38:
-          pressedKeys[KEY_UP] = true;
-          break;
-        case 83:
-        case 40:
-          pressedKeys[KEY_DOWN] = true;
-          break;
-        case 65:
-        case 37:
-          pressedKeys[KEY_LEFT] = true;
-          break;
-        case 68:
-        case 39:
-          pressedKeys[KEY_RIGHT] = true;
-          break;
-      }
-    }, false);
-
-    window.addEventListener('keyup', function (e) {
-      switch (e.keyCode) {
-        case 87:
-        case 38:
-          pressedKeys[KEY_UP] = false;
-          break;
-        case 83:
-        case 40:
-          pressedKeys[KEY_DOWN] = false;
-          break;
-        case 65:
-        case 37:
-          pressedKeys[KEY_LEFT] = false;
-          break;
-        case 68:
-        case 39:
-          pressedKeys[KEY_RIGHT] = false;
-          break;
-      }
-    }, false);
-  })();
+  var controls = new THREE.TrackballControls(renderer.camera);
+  controls.rotateSpeed = 1.0;
+  controls.zoomSpeed = 1.2;
+  controls.panSpeed = 0.8;
+  controls.noZoom = false;
+  controls.noPan = false;
+  controls.staticMoving = true;
+  controls.dynamicDampingFactor = 0.3;
 };
 
 NextPhysics.prototype.constructor = NextPhysics;
--- src/nextphysics/nprenderer/Renderer.js
+++ src/nextphysics/nprenderer/Renderer.js
@@ -13,34 +13,43 @@ NP.Renderer = function(canvasContainer) {
   var renderer = new THREE.WebGLRenderer();
   var scene = new THREE.Scene();
   this.scene = scene;
-  var camera = new THREE.PerspectiveCamera(45, canvasContainer.offsetWidth / canvasContainer.offsetHeight, 0.0001, 100000);
-  var colorSet = NP.ColorSets[0];
   var updateFunctions = [];
 
-  renderer.setClearColor(new THREE.Color(0xEEEEEE));
+  // init renderer
+  renderer.setClearColor(new THREE.Color(0x000000));
   renderer.setSize(canvasContainer.offsetWidth, canvasContainer.offsetHeight);
   canvasContainer.appendChild(renderer.domElement);
-  scene.add(camera);
+
+  var camera = new THREE.PerspectiveCamera(45, canvasContainer.offsetWidth / canvasContainer.offsetHeight, 0.001, 10000);
+  camera.position.z = 500;
+  this.camera = camera;
 
   // Grid
-  var material = new THREE.LineBasicMaterial( { color: 0xa0a0a0} );
-  var geometry = new THREE.Geometry();
-  var floor = -0.04, step = 1, size = 14;
+//  var material = new THREE.LineBasicMaterial( { color: 0xa0a0a0} );
+//  var geometry = new THREE.Geometry();
+//  var floor = -0.04, step = 1, size = 14;
+//  for ( var i = 0; i <= size / step * 2; i ++ ) {
+//    geometry.vertices.push( new THREE.Vector3( - size, floor, i * step - size ) );
+//    geometry.vertices.push( new THREE.Vector3(   size, floor, i * step - size ) );
+//    geometry.vertices.push( new THREE.Vector3( i * step - size, floor, -size ) );
+//    geometry.vertices.push( new THREE.Vector3( i * step - size, floor,  size ) );
+//  }
+//  var line = new THREE.Line( geometry, material, THREE.LinePieces );
+//  scene.add( line );
 
-  for ( var i = 0; i <= size / step * 2; i ++ ) {
-    geometry.vertices.push( new THREE.Vector3( - size, floor, i * step - size ) );
-    geometry.vertices.push( new THREE.Vector3(   size, floor, i * step - size ) );
-    geometry.vertices.push( new THREE.Vector3( i * step - size, floor, -size ) );
-    geometry.vertices.push( new THREE.Vector3( i * step - size, floor,  size ) );
-  }
-  var line = new THREE.Line( geometry, material, THREE.LinePieces );
-  scene.add( line );
+  // lights
+  scene.add(new THREE.AmbientLight(0x333333));
+  var light = new THREE.DirectionalLight(0xffffff, 0.6);
+  light.position.set(5, 3, 5);
+  scene.add(light);
+//  var light = new THREE.PointLight(0xEEEEEE);
+//  light.position.set(20, 0, 20);
+//  scene.add(light);
 
-
-  var axes = new THREE.AxisHelper( 100 );
+  // axes
+  var axes = new THREE.AxisHelper( 1000 );
   scene.add(axes);
 
-  this.camera = camera;
   this.canvas = renderer.domElement;
 
   this.render = function() {
@@ -53,8 +62,7 @@ NP.Renderer = function(canvasContainer) {
 
   this.add = function(object) {
     var renderOptions = {
-      segments: 16,
-      color1: colorSet['color1']
+      segments: 16
     };
 
     object.renderScript(scene, renderOptions, updateFunctions);
